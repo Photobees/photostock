c9282f325371e5c961ab3b86cbb0b9f37ac755be
Sanket Bhaumik <sanket.bhaumik@gmail.com>
2023-01-29 18:58:40 +0100

    Added another env variable

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <div>
-            <div>{process.env.TEST_ENV}</div>
+            <div>{process.env.TEST_MSG}</div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
